838c8d654df127a319bbb1ecb82e4405564b9be5
BarbDMC <barbaradmorantesc@gmail.com>
2024-05-16 23:54:00 -0500

    feat: Add navigation to store

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,7 +1,15 @@
 
+import { useNavigate } from "react-router-dom";
+
 const Logo = () => {
+  const navigate = useNavigate();
+
+  const handleGoStore = () => {
+    navigate('/store');
+  };
+
   return (
-    <div className="bg-red">
+    <div className="bg-red cursor-pointer" onClick={handleGoStore}>
       <h1 className="text-stone-500 text-2xl font-bold font-['Poppins']">Kyn Analytics</h1>
       <p className="text-stone-500 text-base font-light font-['Poppins']">bookstore</p>
     </div>
